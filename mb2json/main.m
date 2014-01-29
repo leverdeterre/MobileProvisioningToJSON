c5cf106e48f7d55bff5d9405bf2c06da6abc2e3e
jerome Morissard <morissardj@gmail.com>
2014-01-29 22:40:11 +0100

    Init

---
+++ mb2json/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  mb2json
+//
+//  Created by Jerome Morissard on 1/29/14.
+//  Copyright (c) 2014 Jerome Morissard. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
